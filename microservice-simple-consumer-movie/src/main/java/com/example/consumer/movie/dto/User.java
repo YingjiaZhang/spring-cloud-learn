907fa1763b0112a82e2047bc528559d462862743
yingjia <zhyingjia@163.com>
2018-08-02 17:58:00 +0800

    [consumer movie] init

---
+++ microservice-simple-consumer-movie/src/main/java/com/example/consumer/movie/dto/User.java
@@ -0,0 +1,20 @@
+package com.example.consumer.movie.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private Long id;
+    private String username;
+    private String name;
+    private Integer age;
+    private BigDecimal balance;
+}
