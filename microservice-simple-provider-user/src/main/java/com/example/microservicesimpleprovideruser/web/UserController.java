446bc632d820868beea1979c8d0e00ecfc91db16
yingjia <zhyingjia@163.com>
2018-08-02 17:09:12 +0800

    [provider user] init

---
+++ microservice-simple-provider-user/src/main/java/com/example/microservicesimpleprovideruser/web/UserController.java
@@ -0,0 +1,22 @@
+package com.example.microservicesimpleprovideruser.web;
+
+import com.example.microservicesimpleprovideruser.dao.UserRepository;
+import com.example.microservicesimpleprovideruser.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/{id}")
+    public Optional<User> findById(@PathVariable Long id) {
+        return userRepository.findById(id);
+    }
+}
