979023ccad278f6a51aeac56005d15fc13aa466e
yingjia <zhyingjia@163.com>
2018-08-03 17:50:00 +0800

    add eureka

---
+++ microservice-provider-user/src/main/java/com/example/provider/user/dao/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.provider.user.dao;
+
+import com.example.provider.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
