0df52ff7b93516aa76696aa8ac2c3de2f375953f
YingjiaZhang <zhyingjia@163.com>
2018-08-05 10:23:16 +0800

    rename microservice-discovery-eureka package

--- microservice-discovery-eureka/src/main/java/pers/zy/springcloud/study/MicroserviceDiscoveryEurekaApplication.java
+++ microservice-discovery-eureka/src/main/java/pers/zy/springcloud/study/MicroserviceDiscoveryEurekaApplication.java
@@ -1,4 +1,4 @@
-package com.example.microservicediscoveryeureka;
+package pers.zy.springcloud.study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
