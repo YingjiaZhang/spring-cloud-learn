fe3c881f800340dc72bb3650a71718fedc40b553
YingjiaZhang <zhyingjia@163.com>
2018-08-05 18:12:57 +0800

    ribbon study

--- microservice-provider-user-my-metadata/src/main/java/pers/zy/springcloud/study/ProviderUserMyMetadataApplication.java
+++ microservice-provider-user-my-metadata/src/main/java/pers/zy/springcloud/study/ProviderUserMyMetadataApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class ProviderUserApplication {
+public class ProviderUserMyMetadataApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProviderUserApplication.class, args);
+		SpringApplication.run(ProviderUserMyMetadataApplication.class, args);
 	}
 }
 
--- microservice-provider-user-my-metadata/src/test/java/pers/zy/springcloud/study/ProviderUserMyMetadataApplicationTests.java
+++ microservice-provider-user-my-metadata/src/test/java/pers/zy/springcloud/study/ProviderUserMyMetadataApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ProviderUserApplicationTests {
+public class ProviderUserMyMetadataApplicationTests {
 
 	@Test
 	public void contextLoads() {
