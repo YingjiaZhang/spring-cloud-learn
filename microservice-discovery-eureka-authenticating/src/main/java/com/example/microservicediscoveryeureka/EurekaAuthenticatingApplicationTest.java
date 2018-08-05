8aedbf70e66ac3e62ce8c920a0ebfb95316baec4
YingjiaZhang <zhyingjia@163.com>
2018-08-05 10:04:24 +0800

    update service discovery and eureka ha

--- microservice-discovery-eureka-authenticating/src/main/java/com/example/microservicediscoveryeureka/EurekaAuthenticatingApplicationTest.java
+++ microservice-discovery-eureka-authenticating/src/main/java/com/example/microservicediscoveryeureka/EurekaAuthenticatingApplicationTest.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class MicroserviceDiscoveryEurekaApplication {
+public class EurekaAuthenticatingApplicationTest {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MicroserviceDiscoveryEurekaApplication.class, args);
+		SpringApplication.run(EurekaAuthenticatingApplicationTest.class, args);
 	}
 }
--- readme.md
+++ readme.md
@@ -1,3 +1,6 @@
+# 参考
+
+- [Spring Boot Actuator: management.security.enabled](https://docs.spring.io/spring-boot/docs/1.2.0.M1/reference/html/production-ready-monitoring.html)
 # 遇到的坑
 
 - spring-cloud-version 不对导致项目启动报错 NoSuchMethodException : 更新版本为 Edgware.RELEASE
