bff9068fec38e31051028e6ce8a07943d06df30b
YingjiaZhang <zhyingjia@163.com>
2018-08-22 09:07:48 +0800

    add config client

--- microservice-config-client/src/main/java/pers/zy/springcloud/study/web/ClientConfigController.java
+++ microservice-config-client/src/main/java/pers/zy/springcloud/study/web/ClientConfigController.java
@@ -1,8 +1,17 @@
 package pers.zy.springcloud.study.web;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ClientConfigController {
 
+    @Value("${profile}")
+    private String profile;
+
+    @GetMapping("/profile")
+    public String getProfile() {
+        return this.profile;
+    }
 }
